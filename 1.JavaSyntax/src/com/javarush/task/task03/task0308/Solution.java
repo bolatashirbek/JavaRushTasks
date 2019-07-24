ccd014d818d50d2d7b98cf215db6e48abc95c5cf
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-07-24 17:23:20 +0600

    Task03.08: Произведение 10 чисел. Вывести на экран произведение 10 чисел от 1 до 10.

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task03.task0308;
+
+/* 
+Произведение 10 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getFactorial(10));
+    }
+
+    public static long getFactorial(int n) {
+        long fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+}
