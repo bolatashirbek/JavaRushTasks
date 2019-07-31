9074f479603d89b3274c3f9162b87e86e68ca47f
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-07-31 18:52:21 +0600

    Task04.12: Положительное и отрицательное число. * Ввести с клавиатуры число. * Если число положительное, то увеличить его в два раза. * Если число отрицательное, то прибавить единицу. * Если введенное число равно нулю, необходимо вывести ноль. * Вывести результат на экран.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0412;
+
+/* 
+Положительное и отрицательное число
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sNumber = reader.readLine();
+        int number = Integer.parseInt(sNumber);
+        if (number > 0) {
+            System.out.println(number * 2);
+        } else if (number < 0) {
+            System.out.println(number + 1);
+        } else {
+            System.out.println(number);
+        }
+    }
+
+}
