d269008f8905b14558c423157bdc89e7f4e7d26a
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-01 15:59:29 +0600

    Task04.19: Максимум четырех чисел. Другие варианты решения.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
@@ -17,6 +17,7 @@ public class Solution {
         maximumOfFourNumbers(first, second, third, fourth);
     }
 
+    // 1-вариант решения: с помощью if else
     private static void maximumOfFourNumbers(int first, int second, int third, int fourth) {
         if ((first >= second) && (first >= third) && (first >= fourth))
             System.out.println(first);
@@ -26,5 +27,23 @@ public class Solution {
             System.out.println(third);
         else
             System.out.println(fourth);
+    }
+
+    // 2-вариант решения: с помощью java.util.Collections. Они так и называются: max и min.
+    private static void maximumOfFourNumbers2(int first, int second, int third, int fourth) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(first);
+        list.add(second);
+        list.add(third);
+        list.add(fourth);
+        System.out.println(Collections.max(list));
+    }
+
+    // 3-вариант решения: с помощью Math.max()
+    private static void maximumOfFourNumbers3(int first, int second, int third, int fourth) {
+        int max1 = Math.max(first, second);
+        int max2 = Math.max(third, fourth);
+        int totalMax = Math.max(max1, max2);
+        System.out.println(totalMax);
     }
 }
