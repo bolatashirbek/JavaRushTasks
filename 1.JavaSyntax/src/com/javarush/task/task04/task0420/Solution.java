6c3ada0f608e92a052463f344110a7b8a5d61fd1
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-02 15:13:07 +0600

    Task04.20: Сортировка трех чисел. - Ввести с клавиатуры три числа, и вывести их в порядке убывания. - Выведенные числа должны быть разделены пробелом.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task04.task0420;
+
+/* 
+Сортировка трех чисел
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        sortThreeNumbers(a, b, c);
+    }
+
+    private static void sortThreeNumbers(int a, int b, int c) {
+        int max = maxOfNumbers(a, b, c);
+        int min = minOfNumbers(a, b, c);
+        int sum = (a + b + c);
+        int mid = sum - (max + min);
+        System.out.println(max + " " + mid + " " + min);
+    }
+
+    private static int maxOfNumbers(int a, int b, int c) {
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else if (c >= a && c >= b)
+            return c;
+        return 0;
+    }
+
+    private static int minOfNumbers(int a, int b, int c) {
+        if (a <= b && a <= c)
+            return a;
+        else if (b <= a && b <= c)
+            return b;
+        else if (c <= a && c <= b)
+            return c;
+        return 0;
+    }
+}
