cd404eceeb6976b07545acd54e32735aff756626
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-04 00:33:02 +0600

    Task04.33: Гадание на долларовый счет. Вывести на экран квадрат из 10х10 букв S используя цикл while. Буквы в каждой строке не разделять.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task04.task0433;
+
+/* 
+Гадание на долларовый счет
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            int k = 0;
+            while (k < 10) {
+                System.out.print("S");
+                k++;
+            }
+
+            System.out.println();
+            i++;
+        }
+
+    }
+}
