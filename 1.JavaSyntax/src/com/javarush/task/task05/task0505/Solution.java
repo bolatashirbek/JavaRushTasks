0d8213227e7c6dfdc3d7380c966381666bb53c56
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-06 22:53:46 +0600

    Task05.05: Кошачья бойня. - Создать три кота используя класс Cat. - Провести три боя попарно между котами. - Класс Cat создавать не надо. Для боя использовать метод boolean fight(Cat anotherCat). - Результат каждого боя вывести на экран c новой строки.

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0505/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task05.task0505;
+
+/* 
+Кошачья бойня
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat barsik = new Cat("Barsik", 3, 4, 5);
+        Cat murzik = new Cat("Murzik", 2, 2, 3);
+        Cat pupkin = new Cat("Pupkin", 4, 5, 5);
+        System.out.println(barsik.fight(murzik));
+        System.out.println(barsik.fight(pupkin));
+        System.out.println(pupkin.fight(murzik));
+    }
+
+    public static class Cat {
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int ageAdvantage = this.age > anotherCat.age ? 1 : 0;
+            int weightAdvantage = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthAdvantage = this.strength > anotherCat.strength ? 1 : 0;
+
+            int score = ageAdvantage + weightAdvantage + strengthAdvantage;
+            return score > 2; // return score > 2 ? true : false;
+        }
+    }
+}
