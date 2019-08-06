3d984412154ad4b79603afa797ca8698b35b3099
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-07 00:33:29 +0600

    Task05.07: Среднее арифметическое. - Вводить с клавиатуры числа и вычислить среднее арифметическое. - Если пользователь ввел -1, вывести на экран среднее арифметическое всех чисел и завершить программу. -1 не должно учитываться.

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task05.task0507;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Среднее арифметическое
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int i = 0;
+        double sum = 0;
+        while (true) {
+            int number = Integer.parseInt(reader.readLine());
+            if (number == -1) {
+                break;
+            }
+            sum += number;
+            i++;
+        }
+        double average = sum / i;
+        System.out.println(average);
+    }
+}
+
