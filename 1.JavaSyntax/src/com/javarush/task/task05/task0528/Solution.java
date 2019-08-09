39f57bb0ec07d4c31adad512801a4dad3774ec74
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-09 19:02:48 +0600

    Task05.28: Вывести на экран сегодняшнюю дату. - Вывести на экран текущую дату в аналогичном виде "21 02 2014".

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task05.task0528;
+
+/* 
+Вывести на экран сегодняшнюю дату
+*/
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Solution {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MM yyyy");
+        String date = sdf.format(new Date());
+        System.out.println(date);
+    }
+}
