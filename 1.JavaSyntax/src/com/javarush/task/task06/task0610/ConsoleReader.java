3dc3b3976eeda05d46fbc6c9e657bc91d7125fd0
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-12 14:54:30 +0600

    Task06.10: Класс ConsoleReader. Сделать класс ConsoleReader, у которого будут 4 статических метода: - String readString() - читает с клавиатуры строку - int readInt() - читает с клавиатуры число - double readDouble() - читает с клавиатуры дробное число - boolean readBoolean() - читает с клавиатуры строку "true" или "false" и возвращает соответствующую логическую переменную true или false   Внимание: создавайте переменную для чтения данных с консоли (BufferedReader или Scanner) внутри каждого метода.

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task06.task0610;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Класс ConsoleReader
+*/
+
+public class ConsoleReader {
+    public static String readString() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String result = reader.readLine();
+        return result;
+    }
+
+    public static int readInt() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        return number;
+    }
+
+    public static double readDouble() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double dNumber = Double.parseDouble(reader.readLine());
+        return dNumber;
+    }
+
+    public static boolean readBoolean() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean bool = Boolean.parseBoolean(reader.readLine());
+        return bool;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(readString());
+        System.out.println(readInt());
+        System.out.println(readDouble());
+        System.out.println(readBoolean());
+    }
+}
