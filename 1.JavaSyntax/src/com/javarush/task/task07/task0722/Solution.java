4eb519428f5db49f55959bec521436eaaf8a19fa
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-16 17:25:43 +0600

    Task07.22: Это конец. - Создать список строк. - Ввести строки с клавиатуры и добавить их в список. - Вводить с клавиатуры строки, пока пользователь не введет строку "end". Саму строку "end" не учитывать. - Вывести строки на экран, каждую с новой строки.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0722/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task07.task0722;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Это конец
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (; ; ) {
+            String text = reader.readLine();
+            if (text.equals("end")) break;
+            list.add(text);
+        }
+
+        for (String elem : list) System.out.println(elem);
+    }
+}
