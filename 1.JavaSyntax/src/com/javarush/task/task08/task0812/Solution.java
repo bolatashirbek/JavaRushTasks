f09fa23083b4d47c30959d9cfe8fe650530dba4e
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-19 00:34:50 +0600

    Task08.12: Cамая длинная последовательность. 1. Создай список чисел. 2. Добавь в список 10 чисел с клавиатуры. 3. Вывести на экран длину самой длинной последовательности повторяющихся чисел в списке.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task08.task0812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Cамая длинная последовательность
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int number = Integer.parseInt(reader.readLine());
+            list.add(number);
+        }
+
+        int count = 1;
+        int current = 1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                ++current;
+                if (current > count) count = current;
+            } else current = 1;
+        }
+        System.out.println(count);
+    }
+}
