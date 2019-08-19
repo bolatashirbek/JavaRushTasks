836dd8b8d7161d4ea0bb797cf171c20a67ce7017
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-19 10:28:51 +0600

    Task08.13: 20 слов на букву «Л». - Создать множество строк (Set<String>), занести в него 20 слов на букву "Л".

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<>();
+        set.add("Луна");
+        set.add("Линейка");
+        set.add("Лайка");
+        set.add("Лагерь");
+        set.add("Луч");
+        set.add("Лицо");
+        set.add("Лес");
+        set.add("Лев");
+        set.add("Ложь");
+        set.add("Лист");
+        set.add("Лиса");
+        set.add("Лондон");
+        set.add("Лед");
+        set.add("Лежать");
+        set.add("Лодка");
+        set.add("Лук");
+        set.add("Летчик");
+        set.add("Лето");
+        set.add("Ложка");
+        set.add("Литература");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
