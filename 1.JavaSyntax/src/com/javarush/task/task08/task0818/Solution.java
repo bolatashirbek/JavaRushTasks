ecd167acb691e85faf7b49ac50cea892879df938
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-19 12:51:34 +0600

    Task08.18: Только для богачей. - Создать словарь (Map<String, Integer>) и занести в него десять записей по принципу: "фамилия" - "зарплата". - Удалить из словаря всех людей, у которых зарплата ниже 500.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0818;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/* 
+Только для богачей
+*/
+
+public class Solution {
+    public static Map<String, Integer> createMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Adilbek", 800);
+        map.put("Tausogar", 900);
+        map.put("Jaibasar", 300);
+        map.put("Tasbaka", 200);
+        map.put("Marat", 1500);
+        map.put("Armanuly", 1200);
+        map.put("Joldasbek", 250);
+        map.put("Pazylbek", 2500);
+        map.put("Bazarbek", 3000);
+        map.put("Ermekov", 400);
+        return map;
+    }
+
+    public static void removeItemFromMap(Map<String, Integer> map) {
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> pair = iterator.next();
+            if (pair.getValue() < 500) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
