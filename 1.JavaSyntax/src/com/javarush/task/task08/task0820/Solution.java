0944d0da21a820ffa83dde68fa8f8ecf96e3e682
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-21 11:27:25 +0600

    Task08.20: Множество всех животных. 1. Внутри класса Solution создать public static классы Cat, Dog без конструктора или с конструктором без параметров. 2. Реализовать метод createCats, который должен возвращать множество с 4 котами. 3. Реализовать метод createDogs, который должен возвращать множество с 3 собаками. 4. Реализовать метод join, который должен возвращать объединенное множество всех животных - всех котов и собак. 5. Реализовать метод removeCats, который должен удалять из множества pets всех котов, которые есть в множестве cats. 6. Реализовать метод printPets, который должен выводить на экран всех животных, которые в нем есть. Каждое животное с новой строки.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0820/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task08.task0820;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Множество всех животных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            result.add(new Cat());
+        }
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        Set<Dog> dogs = new HashSet<>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        return dogs;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        Set<Object> pets = new HashSet<>();
+        pets.addAll(cats);
+        pets.addAll(dogs);
+        return pets;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        Iterator<Object> petsIterator = pets.iterator();
+        while (petsIterator.hasNext()) {
+            Object pet = petsIterator.next();
+            for (Cat cat : cats) {
+                if (pet.equals(cat)) {
+                    petsIterator.remove();
+                }
+            }
+        }
+    }
+
+    public static void printPets(Set<Object> pets) {
+        for (Object pet : pets) System.out.println(pet);
+    }
+
+    public static class Cat {
+        public Cat() {
+        }
+    }
+
+    public static class Dog {
+        public Dog() {
+        }
+    }
+}
