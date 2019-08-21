ce8eccc9e4b2689ab2d163dc36b11afc8c113a85
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-21 10:41:16 +0600

    Task08.19: Set из котов. 1. Внутри класса Solution создать public static класс кот - Cat. 2. Реализовать метод createCats, он должен создавать множество (Set) котов и добавлять в него 3 кота. 3. В методе main удалите одного кота из Set cats. 4. Реализовать метод printCats, он должен вывести на экран всех котов, которые остались во множестве. Каждый кот с новой строки.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task08.task0819;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Set из котов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+
+        Iterator<Cat> iterator = cats.iterator();
+        while (iterator.hasNext()) {
+            cats.remove(iterator.next());
+            break;
+        }
+
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> cats = new HashSet<>();
+        cats.add(new Cat());
+        cats.add(new Cat());
+        cats.add(new Cat());
+        return cats;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        for (Cat cat : cats) {
+            System.out.println(cat);
+        }
+    }
+
+    public static class Cat {
+        public Cat() {
+        }
+    }
+}
