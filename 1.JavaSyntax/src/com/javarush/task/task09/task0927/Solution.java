136eb972c035e287d29d7571518665690579ef14
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-23 12:36:28 +0600

    Task09.27: Десять котов. - Есть класс кот - Cat, с полем "имя" (String). - Создать словарь Map<String, Cat> и добавить туда 10 котов в виде "Имя"-"Кот". - Получить из Map множество(Set) всех котов и вывести его на экран.

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0927/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task09.task0927;
+
+import java.util.*;
+
+/* 
+Десять котов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, Cat> map = createMap();
+        Set<Cat> set = convertMapToSet(map);
+        printCatSet(set);
+    }
+
+    public static Map<String, Cat> createMap() {
+        Map<String, Cat> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            String cat = "cat" + i;
+            map.put(cat, new Cat(cat));
+        }
+        return map;
+    }
+
+    public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
+        Set<Cat> cats = new HashSet<>();
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            cats.add(pair.getValue());
+        }
+        return cats;
+    }
+
+    public static void printCatSet(Set<Cat> set) {
+        for (Cat cat : set) {
+            System.out.println(cat);
+        }
+    }
+
+    public static class Cat {
+        private String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return "Cat " + this.name;
+        }
+    }
+
+
+}
