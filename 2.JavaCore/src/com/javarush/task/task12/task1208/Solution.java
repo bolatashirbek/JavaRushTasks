3c985ff80682a8fa173cdd25e5d50d69f71c9b6d
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-08-29 18:43:08 +0600

    Task12.08: Свобода печати. - Написать пять методов print с разными параметрами.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int number) {
+    }
+
+    public static void print(String string) {
+    }
+
+    public static void print(char c) {
+    }
+
+    public static void print(boolean b) {
+    }
+
+    public static void print(Double d) {
+    }
+}
