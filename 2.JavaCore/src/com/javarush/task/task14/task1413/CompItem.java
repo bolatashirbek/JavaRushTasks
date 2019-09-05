39354fad200f701ba309296008eac57426ae1b58
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-09-05 17:30:34 +0600

    Task14.13: Computer. 1. Создай интерфейс CompItem. 2. Добавь в него метод String getName(). 3. Создай классы Keyboard, Mouse, Monitor, которые реализуют интерфейс CompItem. 4. Метод getName() должен возвращать имя класса, например, для класса Keyboard будет "Keyboard". 5. Создай класс Computer. 6. В класс Computer добавь приватное поле keyboard типа Keyboard. 7. В класс Computer добавь приватное поле mouse типа Mouse. 8. В класс Computer добавь приватное поле monitor типа Monitor. 9. Создай конструктор с тремя параметрами в классе Computer используя комбинацию клавиш Alt+Insert внутри класса (команда Constructor). 10. Внутри конструктора инициализируйте все три поля (переменных) класса в соответствии с переданными параметрами. 11. Создай геттеры для полей класса Computer (в классе используй комбинацию клавиш Alt+Insert и выбери команду Getter). 12. Все созданные классы и интерфейс должны быть в отдельных файлах.

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/CompItem.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task14.task1413;
+
+/**
+ * Created by Bolat Ashirbek on 05.09.2019.
+ */
+
+public interface CompItem {
+    String getName();
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Monitor.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task14.task1413;
+
+/**
+ * Created by Bolat Ashirbek on 05.09.2019.
+ */
+
+public class Monitor implements CompItem {
+    @Override
+    public String getName() {
+        return "Monitor";
+    }
+}
