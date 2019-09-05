f1bfc6a5fc95dbc1faabcf0ca1dcb16aae4a9ba0
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-09-05 16:52:53 +0600

    Task14.11: User, Loser, Coder and Proger. 1. Ввести [в цикле] с клавиатуры несколько строк (ключей). - Строки(ключи) могут быть такими: "user", "loser", "coder", "proger". - Ввод окончен, когда строка не совпадает ни с одной из выше указанных. 2. Для каждой введенной строки нужно: 2.1. Создать соответствующий объект [см. Person.java], например, для строки "user" нужно создать объект класса User. 2.2. Передать этот объект в метод doWork. 3. Написать реализацию метода doWork, который: 3.1. Вызывает метод live() у переданного объекта, если этот объект (person) имеет тип User. 3.2. Вызывает метод doNothing(), если person имеет тип Loser. 3.3. Вызывает метод writeCode(), если person имеет тип Coder. 3.4. Вызывает метод enjoy(), если person имеет тип Proger.

--- 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
@@ -4,7 +4,7 @@ package com.javarush.task.task14.task1410;
  * Created by Bolat Ashirbek on 05.09.2019.
  */
 public class BubblyWine extends Wine {
-    public String getHolidayName(){
+    public String getHolidayName() {
         return "Новый Год";
     }
 }
