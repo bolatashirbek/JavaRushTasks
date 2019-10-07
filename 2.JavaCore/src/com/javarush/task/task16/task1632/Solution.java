fd0962bee4e046574149f28b3acfee6b4dd5e513
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-10-07 18:48:33 +0600

    Task16.32: Клубок. 1. Создай 5 различных своих нитей c отличным от Thread типом: 1.1. Нить 1 должна бесконечно выполняться; 1.2. Нить 2 должна выводить "InterruptedException" при возникновении исключения InterruptedException; 1.3. Нить 3 должна каждые полсекунды выводить "Ура"; 1.4. Нить 4 должна реализовать интерфейс Message, при вызове метода showWarning нить должна останавливаться; 1.5. Нить 5 должна читать с консоли числа пока не введено слово "N", а потом вывести в консоль сумму введенных чисел. 2. В статическом блоке добавь свои нити в List<Thread> threads в перечисленном порядке. 3. Нити не должны стартовать автоматически.

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
@@ -0,0 +1,94 @@
+package com.javarush.task.task16.task1632;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static List<Thread> threads = new ArrayList<>(5);
+
+    static {
+        threads.add(new ThreadOne());
+        threads.add(new ThreadTwo());
+        threads.add(new ThredThree());
+        threads.add(new ThreadFour());
+        threads.add(new ThreadFive());
+    }
+
+    public static void main(String[] args) {
+        ThreadFour t = new ThreadFour();
+        t.run();
+    }
+
+    public static class ThreadOne extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+            }
+        }
+    }
+
+    public static class ThreadTwo extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException");
+            }
+        }
+    }
+
+    public static class ThredThree extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("Ура");
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class ThreadFour extends Thread implements Message {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+            }
+        }
+
+        @Override
+        public void showWarning() {
+            threads.get(3).stop();
+        }
+    }
+
+    public static class ThreadFive extends Thread {
+        private String text;
+        private volatile int result;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    text = reader.readLine();
+                    if (text.equals("N")) {
+                        System.out.println(result);
+                        Thread.currentThread().interrupt();
+                        break;
+                    }
+                    result += Integer.parseInt(text);
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
