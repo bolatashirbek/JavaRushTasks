a69dd43f24c8612766e6bd49f0230b78534339c4
Bolat Ashirbek <bolat.89arys@mail.ru>
2020-03-07 00:27:52 +0600

    Task17.13.

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1713/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task17.task1713;
+
+/* 
+Общий список
+*/
+
+import java.util.ArrayList;
+
+public class Solution {
+    private ArrayList<Long> original = new ArrayList<Long>();
+
+    public static void main(String[] args) {
+
+    }
+}
