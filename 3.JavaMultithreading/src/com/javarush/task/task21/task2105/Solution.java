5a5db7224e250f9ea50c4e21d720523551ecab54
Bolat Ashirbek <bolat.89arys@mail.ru>
2020-03-08 23:27:02 +0600

    Task21.05. Исправить ошибку. Сравнение объектов. 1. Хешкоды одинаковых объектов должны быть равны. 2. Метод equals должен проверять равен ли переданный объект равен текущему(сравнение через ==). 3. Метод equals должен проверять является ли переданный объект объектом класса Solution. 4. Метод equals должен возвращать true в случае, если поля first и last равны у переданного объекта и текущего(не забудь что они могут быть равны null). 5. Должно быть обеспечено корректное поведение HashSet с типом элементов Solution. 6. В классе Solution должен быть реализован метод hashCode.

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -14,11 +14,22 @@ public class Solution {
         this.last = last;
     }
 
+    @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Solution))
-            return false;
+        if (this == o) return true;
+        if (!(o instanceof Solution)) return false;
+
         Solution n = (Solution) o;
-        return n.first.equals(first) && n.last.equals(last);
+
+        if (first != null ? !first.equals(n.first) : n.first != null) return false;
+        return last != null ? last.equals(n.last) : n.last == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
     }
 
     public static void main(String[] args) {
