a5be1ed6870bb20438b092b7c8c0d7f48eb9df9c
Bolat Ashirbek <bolat.89arys@mail.ru>
2020-03-08 16:05:57 +0600

    Task20.01-27. Without solutions.

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task20.task2003;
+
+/* 
+Знакомство с properties
+*/
+public class Solution {
+    public static Map<String, String> properties = new HashMap<>();
+
+    public void fillInPropertiesMap() {
+        //implement this method - реализуйте этот метод
+    }
+
+    public void save(OutputStream outputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+    }
+
+    public void load(InputStream inputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
