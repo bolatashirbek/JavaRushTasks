2e79a711b0ef1fd2595eafa8c8123841e28e3f8f
Bolat Ashirbek <bolat.89arys@mail.ru>
2020-03-08 16:44:51 +0600

    Task21.13. Ипподром(6). 1. В классе Hippodrome должно быть создано поле game типа Hippodrome. 2. Поле game должно быть статическим. 3. Поле game НЕ должно быть приватным. 4. В методе main должно инициализироваться поле game. 5. В методе main в список лошадей ипподрома должны быть добавлены три лошади. 6. Скорости всех лошадей должны быть равны 3, а дистанции - 0.

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -4,12 +4,19 @@ import java.util.*;
 
 public class Hippodrome {
     private List<Horse> horses;
+    public static Hippodrome game;
 
     public static void main(String[] args) {
-
+        game = new Hippodrome(new ArrayList<>());
+        Horse akbozat = new Horse("Ақбозат", 3, 0);
+        Horse kulager = new Horse("Құлагер", 3, 0);
+        Horse naizagai = new Horse("Найзағай", 3, 0);
+        game.getHorses().add(akbozat);
+        game.getHorses().add(kulager);
+        game.getHorses().add(naizagai);
     }
 
-    public Hippodrome(List list) {
+    public Hippodrome(List<Horse> list) {
         this.horses = list;
     }
 
