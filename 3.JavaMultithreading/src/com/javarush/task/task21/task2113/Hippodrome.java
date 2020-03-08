685fb30f3907c8cc04307d805b5040579af566c5
Bolat Ashirbek <bolat.89arys@mail.ru>
2020-03-08 16:47:28 +0600

    Task21.13. Ипподром(7). 1. В классе Hippodrome должен быть создан метод run без параметров. 2. В классе Hippodrome должен быть создан метод move без параметров. 3. В классе Hippodrome должен быть создан метод print без параметров.

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -23,4 +23,13 @@ public class Hippodrome {
     public List<Horse> getHorses() {
         return horses;
     }
+
+    public void run() {
+    }
+
+    public void move() {
+    }
+
+    public void print() {
+    }
 }
